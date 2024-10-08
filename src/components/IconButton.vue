9e5a9b5d0405d325cf1c657ec6affbf313e5187b
sadddge <m.sanhueza22@ufromail.cl>
2024-10-08 05:06:04 -0300

    Create IconButton.vue

---
+++ src/components/IconButton.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="button">
+        <img v-if="image" :src="getImage(image)" alt="">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { defineProps } from 'vue';
+    defineProps({
+        image : {
+            type: String,
+            default: ''
+        }
+    });
+
+    const getImage = (imageName : string) => {
+        try {
+            return require(`../assets/images/${imageName}`);
+        } catch (error) {
+            console.error(`Image ${imageName} not found`);
+            return "";
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .button {
+        display: flex;
+        width: 50px;
+        height: 50px;
+        background-color: $button-color;
+        color: $button-text-color;
+        padding: 0;
+        border-radius: 5px;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+
+        &:hover {
+            background-color: $button-hover-color;
+        }
+
+        &:active {
+            background-color: $button-active-color;
+        }
+
+        img {
+            width: 40px;
+            height: 40px;
+        }
+    }
+</style>
