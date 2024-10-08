7710369089ab7ce013543cadfd4f9e7c45def28a
sadddge <m.sanhueza22@ufromail.cl>
2024-10-08 00:51:04 -0300

    Crear SideBarButton.vue
    Se crea el componente para los botones de la sidebar, tienen un icono como prop el cual si no se especifica no se dibuja.
    Tambien se añadieron los svg usados en el avance 2.

---
+++ src/components/SideBarButton.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="button">
+        <img v-if="imageName" :src="getImage(imageName)" alt="">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { defineProps} from 'vue';
+
+    defineProps({
+        imageName : {
+            type: String,
+            default : ""
+        } 
+    });
+
+    const getImage = (imageName : string) => {
+        return require(`../assets/images/${imageName}`);
+    }
+
+</script>
+
+<style lang="scss">
+    .button {
+        display: flex;
+        gap: 7px;
+        align-items: center;
+        border-radius: 5px;
+        padding: 5px 10px;
+        cursor: pointer;
+
+        &:hover {
+            background-color: $button-hover-color;
+        }
+
+        &:active {
+            background-color: $button-active-color;
+        }
+    }
+</style>
