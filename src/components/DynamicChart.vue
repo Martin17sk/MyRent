e7b347d82920ffc88210d01873e57fd6fa4b0f32
sadddge <m.sanhueza22@ufromail.cl>
2024-10-18 20:24:48 -0300

    feat: añadir componentes de graficos
    Añadir componentes de los graficos junto con sus dependencias.

---
+++ src/components/DynamicChart.vue
@@ -0,0 +1,35 @@
+<template>
+    <component :is="component" :data="data"/>
+</template>
+
+<script setup>
+import BarChart from './BarChart.vue';
+import LineChart from './LineChart.vue';
+import PieChart from './PieChart.vue';
+import { computed } from 'vue';
+
+    const props = defineProps({
+        currentComponent: {
+            type: String,
+            default: 'Bar',
+        },
+        data: Object,
+    });
+
+    const component = computed(() => {
+        switch (props.currentComponent) {
+            case 'Bar':
+                return BarChart;
+            case 'Line':
+                return LineChart;
+            case 'Pie':
+                return PieChart;
+            default:
+                return BarChart;
+        }
+    });
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/PieChart.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <Pie :data="data" :options="options"/>
+    </div>
+</template>
+
+<script setup>
+    import { Pie } from 'vue-chartjs';
+    import { Chart, registerables } from 'chart.js';
+    import { ref } from 'vue';
+    Chart.register(...registerables);
+    defineProps({
+        data : Object,
+    });
+
+    const options = ref({
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    });
+
+</script>
+
+<style lang="scss" scoped>
+    div {
+        width: 500px;
+    }
+</style>
