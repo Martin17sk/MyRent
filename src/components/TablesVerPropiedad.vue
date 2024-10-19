2fb1476692b110debd520a0d45d1bb3028bc6cd9
Martin Muñoz <martin.andres.munoz.c@gmail.com>
2024-10-19 13:33:01 -0300

    feat: componente tablas a usar en VerPropiedadView
    Se creó el componente TablesVerPropiedad.vue el cual será utlizada en VerPropiedadView.vue

---
+++ src/components/TablesVerPropiedad.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="tables-container">
+        <div>
+            <h2>Inventario:</h2>
+            <DefaultTable :columns="inventoryColumns" :data="inventoryData" :widthProp="'300px'" />
+        </div>
+
+        <div>
+            <h2>Trabajadores:</h2>
+            <DefaultTable :columns="workersColumns" :data="workersData" :widthProp="'500px'" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import DefaultTable from './DefaultTable.vue';
+
+// Example data from db.json for a specific property
+const inventoryData = ref([
+    { name: "Sartén", quantity: 2 },
+    { name: "Olla", quantity: 1 },
+    { name: "Cuchara", quantity: 3 },
+    { name: "Tenedor", quantity: 3 },
+    { name: "Plato", quantity: 3 },
+]);
+
+const workersData = ref([
+    { name: "Sebastian", service: "Limpieza", salary: 100 },
+]);
+
+const inventoryColumns = ref([
+    { label: 'Nombre', key: 'name' },
+    { label: 'Cantidad', key: 'quantity' },
+]);
+
+const workersColumns = ref([
+    { label: 'Nombre', key: 'name' },
+    { label: 'Servicio', key: 'service' },
+    { label: 'Sueldo', key: 'salary' },
+]);
+</script>
+
+<style scoped>
+.tables-container {
+    display: flex;
+    flex-direction: row;
+    gap: 50px;
+    margin: 20px;
+}
+</style>
