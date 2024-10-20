6bf336d4b31a1a1fa0d8d54165e9bae19e2fb56f
SebaSinMas <sebastianbustos90@gmail.com>
2024-10-20 15:38:27 -0300

    Chore: Inicializa los archivos de Register y Recover

---
+++ src/views/RecoverPasswordView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+    <h1>Aguante el albo campeón</h1>
+</template>
+<style lang="scss" scoped></style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+<template>
+    <h1>tangamandapio</h1>
+</template>
+<style lang="scss" scoped></style>
