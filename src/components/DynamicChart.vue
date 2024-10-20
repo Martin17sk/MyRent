9d1da92ff73f2544ee49069b869553ffc7b46b19
sadddge <m.sanhueza22@ufromail.cl>
2024-10-20 20:15:23 -0300

    fix: eliminar caso default en DynamicChart
    Eliminar caso default innecesario.

--- src/components/DynamicChart.vue
+++ src/components/DynamicChart.vue
@@ -24,8 +24,6 @@ import { computed } from 'vue';
                 return LineChart;
             case 'Pie':
                 return PieChart;
-            default:
-                return BarChart;
         }
     });
 </script>
