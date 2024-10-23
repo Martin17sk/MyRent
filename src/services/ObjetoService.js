443b4cfa600b66422a0aabe82f2007cc13b0f177
Martin Muñoz <martin.andres.munoz.c@gmail.com>
2024-10-22 22:20:20 -0300

    feat: crear servicio de objetos de inventario
    crear ObjetoService.js que permite la gestion de llamados al json server acerca de los objetos del inventario

---
+++ src/services/ObjetoService.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+class ObjetoService{
+    async getAllObjetos(){
+        const response = await axios.get('http://localhost:3000/objetos');
+        const objetos = response.data;
+        return objetos;
+    }
+
+    async getObjetosByName(name){
+        const response = await axios.get(`http://localhost:3000/objetos?name=${name}`);
+        return response.data;
+    }
+
+    async getObjetoById(id){
+        const response = await axios.get(`http://localhost:3000/objetos?id=${id}`);
+        return response.data;
+    }
+
+    async addObjeto(objeto){
+        const objetoExists = await this.getObjetosByName(objeto.nombre);
+        if(objetoExists.length > 0){
+            console.log(objeto.nombre + "already exists");
+        } else {
+            const response = await axios.post('http://localhost:3000/objetos', objeto);
+            return response.data;
+        }
+        return {};
+    }
+
+    async checkIfObjetoExists(objeto){
+        const response = await axios.get(`http://localhost:3000/objetos?name=${objeto.nombre}`);
+        return response.data.length > 0;
+    }
+
+    async updateObjeto(objeto){
+        const response = await axios.put(`http://localhost:3000/objetos/${objeto.id}`, objeto);
+        return response.data;
+    }
+
+    async deleteObjeto(id){
+        const response = await axios.delete(`http://localhost:3000/objetos/${id}`);
+        return response.data;
+    }
+}
+export default ObjetoService;
