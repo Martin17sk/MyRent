57c0008b77242cc0df850cd06cf0f465fd6f944d
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2024-11-04 02:25:36 -0300

    chore: añade métodos necesarios para usuario

--- backend/src/main/java/io/github/MyRent/myrent/repository/UsuarioRepository.java
+++ backend/src/main/java/io/github/MyRent/myrent/repository/UsuarioRepository.java
@@ -2,8 +2,20 @@ package io.github.MyRent.myrent.repository;
 
 import io.github.MyRent.myrent.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByIdUsuario(Long id);
+    Optional<Usuario> findByIdUsuario(int id);
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO usuario(`correo`, `contrasenia`) values (:correo, :contrasenia)", nativeQuery = true)
+    void insertarUsuario(@Param("correo") String correo,@Param("contrasenia") String contrasenia);
+
 }
