4f0f9caa5dbe67f7883ccf00f501829cb0b7146d
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2024-11-23 03:53:34 -0300

    refactor: Axios se comunica al backend

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -3,29 +3,30 @@ import axios from 'axios'
 import { useUserStore } from './user'
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    user: null, // Aquí almacenaremos los datos del usuario autenticado
+    user: null,
     isAuthenticated: false,
     error: null,
   }),
   actions: {
     async login(email, password) {
       try {
-        const response = await axios.get(
-          `http://localhost:3000/users?correo=${email}&contraseña=${password}`,
+        const response = await axios.post(
+          `http://localhost:8005/auth/login`, {
+            "correo": email,
+            "contrasenia":password
+          }
         )
-
-        const user = response.data[0] // Encontrar al usuario con las credenciales
-
-        if (user) {
-          this.user = user
+        console.log(response);
+        if (response.status === 200) {
+          this.user = response.data
+          useUserStore().setUserId(this.user.id_usuario)
           this.isAuthenticated = true
-          useUserStore().setUserId(user.id) //Ojo acá por que me puedo equivocar
-          localStorage.setItem('user', JSON.stringify(user))
-        } else {
-          this.error = 'Usuario o contraseña incorrectos'
+          localStorage.setItem('user', JSON.stringify(response.data))
+        }else{
+          this.error = 'Hubo un problema con la autenticación'
           this.isAuthenticated = false
         }
-      } catch (error) {
+      }catch (error) {
         this.error = 'Hubo un problema con la autenticación'
         this.isAuthenticated = false
         console.log(error)
@@ -33,7 +34,7 @@ export const useAuthStore = defineStore('auth', {
     },
 
     logout() {
-      this.user = null
+    
       this.isAuthenticated = false
       localStorage.removeItem('user') // Limpiar la sesión
     },
@@ -42,7 +43,7 @@ export const useAuthStore = defineStore('auth', {
       const userData = localStorage.getItem('user')
       if (userData) {
         this.user = JSON.parse(userData)
-        useUserStore().setUserId(this.user.id) //Puede que esto genere problemas ojo para mañana
+        useUserStore().setUserId(this.user.id_usuario) //Puede que esto genere problemas ojo para mañana
         this.isAuthenticated = true
       }
     },
